75e85191c61278c488db1aa4d59310170ca3c4f1
Tal Shiri <eiopaa@gmail.com>
2018-01-02 18:34:48 -0800

    Speedup reads by using mmap()

--- noisereduction/Utils.h
+++ noisereduction/Utils.h
@@ -8,4 +8,15 @@ struct SndContext {
     SF_INFO info;
 };
 
+class SndMmap {
+    sf_count_t length;
+    int fd;
+    char* mmappedData;
+    sf_count_t position;
+public:
+    SF_VIRTUAL_IO interface;
+    SndMmap(const char* path);
+    SndContext Open();
+};
+
 SndContext openAudioFile(const char* path);

1231564b841acfe80c8868c7cf31dff27c277085
Jack-42 <jakob-robert@hotmail.de>
2020-10-21 00:15:56 +0200

    Extend CLI to provide separate track for profiling
    Add new tests as well

--- noisereduction/main.cpp
+++ noisereduction/main.cpp
@@ -12,8 +12,9 @@ int main(int argc, char * argv[]) {
     options.add_options()
         ("i,input", "Input file (required)", cxxopts::value<std::string>())
         ("o,output", "Output file (required)", cxxopts::value<std::string>())
+        ("p,profile", "File for noise profiling (must contain only noise)", cxxopts::value<std::string>())
         ("t0", "Start sample for noise profiling", cxxopts::value<size_t>())
-        ("t1", "end sample for noise profiling", cxxopts::value<size_t>())
+        ("t1", "End sample for noise profiling", cxxopts::value<size_t>())
         ("noiseGain", "Noise Gain (dB)", cxxopts::value<float>()->default_value("12"))
         ("sensitivity", "Sensitivity", cxxopts::value<float>()->default_value("6.0"))
         ("smoothing", "Frequency Smoothing (bands)", cxxopts::value<int>()->default_value("0"))
@@ -39,33 +40,44 @@ int main(int argc, char * argv[]) {
 
     std::cout << "Processing " << result["input"].as<std::string>() << " -> " << result["output"].as<std::string>() << std::endl;
 
-    SndContext ctx = openAudioFile(result["input"].as<std::string>().c_str());
+    SndContext inputCtx = openAudioFile(result["input"].as<std::string>().c_str());
 
     NoiseReduction::Settings settings;
     settings.mNewSensitivity = result["sensitivity"].as<float>();
     settings.mFreqSmoothingBands = result["smoothing"].as<int>();
     settings.mNoiseGain = result["noiseGain"].as<float>();
 
-    NoiseReduction reduction(settings, ctx.info.samplerate);
-
-    size_t t0 = 0;
-    size_t t1 = ctx.info.frames;
-    if (result["t0"].count()) {
-        t0 = result["t0"].as<size_t>();
-    }
-
-    if (result["t1"].count()) {
-        t1 = result["t1"].as<size_t>();
-    }
+    NoiseReduction reduction(settings, inputCtx.info.samplerate);
 
     std::cout << "Profiling noise..." << std::endl;
-    std::vector<InputTrack> profileTracks = TrackUtils::readTracksFromContext(ctx, t0, t1);
+
+    std::vector<InputTrack> profileTracks;
+
+    if (result.count("profile")) {
+        // use separate track for profiling noise
+        SndContext profileCtx = openAudioFile(result["profile"].as<std::string>().c_str());
+        profileTracks = TrackUtils::readTracksFromContext(profileCtx);
+    } else {
+        // use time indices for profiling noise
+        size_t t0 = 0;
+        size_t t1 = inputCtx.info.frames;
+        if (result["t0"].count()) {
+            t0 = result["t0"].as<size_t>();
+        }
+
+        if (result["t1"].count()) {
+            t1 = result["t1"].as<size_t>();
+        }
+        
+        profileTracks = TrackUtils::readTracksFromContext(inputCtx, t0, t1);
+    }
+
     for (auto& profileTrack : profileTracks) {
         reduction.ProfileNoise(profileTrack);
     }
 
-    std::cout << "Denoising..." << std::endl;
-    std::vector<InputTrack> inputTracks = TrackUtils::readTracksFromContext(ctx);
+    std::cout << "Reducing noise..." << std::endl;
+    std::vector<InputTrack> inputTracks = TrackUtils::readTracksFromContext(inputCtx);
     std::vector<OutputTrack> outputTracks;
     for (auto& inputTrack : inputTracks) {
         OutputTrack outputTrack;
@@ -74,7 +86,7 @@ int main(int argc, char * argv[]) {
     }
     
     const char* outputPath = result["output"].as<std::string>().c_str();
-    TrackUtils::writeTracksToFile(outputPath, outputTracks, ctx.info.channels, ctx.info.samplerate);
+    TrackUtils::writeTracksToFile(outputPath, outputTracks, inputCtx.info.channels, inputCtx.info.samplerate);
 
     return 0;
 }

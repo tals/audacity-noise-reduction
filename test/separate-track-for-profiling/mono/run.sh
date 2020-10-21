0031f596bed29f4916225610cd10a78377f1bf90
Jack-42 <jakob-robert@hotmail.de>
2020-10-22 01:05:41 +0200

    Extend CLI to provide separate track for profiling (#9)
    Add new tests as well

---
+++ test/separate-track-for-profiling/mono/run.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+input=./square-with-noise.wav
+output=./square-with-noise-output.wav
+profile=./noise-profile.wav
+gain=24
+sensitivity=12
+smoothing=0
+../../../bin/noisereduction_driver -i $input -o $output -p $profile --noiseGain $gain --sensitivity $sensitivity --smoothing $smoothing

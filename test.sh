7eccdf7b9804c20ebc49549b6c70d188739d3c46
Jack-42 <jakob-robert@hotmail.de>
2020-11-22 23:15:13 +0100

    Remove TODO

--- test.sh
+++ test.sh
@@ -3,6 +3,5 @@ mkdir -p build
 cd build
 cmake ..
 cmake --build . --target test_runner
-:: TODO: clean up: better prepend this path in source code and run directly from build directory
 cd ../samples
 ../build/test_runner

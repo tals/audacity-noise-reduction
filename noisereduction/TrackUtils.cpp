be9c324fa4be692197b9bbaf9b7618e762a978d5
Jack-42 <jakob-robert@hotmail.de>
2020-11-24 21:21:07 +0100

    Use unique_ptr for frameBuffer

--- noisereduction/TrackUtils.cpp
+++ noisereduction/TrackUtils.cpp
@@ -29,13 +29,12 @@ InputTrack TrackUtils::readOneTrackFromContext(const SndContext &ctx, int channe
     sf_seek(ctx.file, t0, SEEK_SET);
     float* writePtr = &buffer[0];
 
-    // allocating on heap because variable size stack arrays are evil!
-    float* frameBuffer = new float[ctx.info.channels];
+    auto frameBuffer = std::make_unique<float[]>(ctx.info.channels);
 
     // can only read full frames from libsnd.
     // will probably be a lot faster to read the whole thing and then split it
     for (size_t frame = t0; frame < t1; frame++) {
-        size_t read = sf_readf_float(ctx.file, frameBuffer, 1);
+        size_t read = sf_readf_float(ctx.file, frameBuffer.get(), 1);
 
         if (read == 0) {
             LOG_F(WARNING, "Couldn't read data from input file");
@@ -45,8 +44,6 @@ InputTrack TrackUtils::readOneTrackFromContext(const SndContext &ctx, int channe
         *writePtr = frameBuffer[channel];
         writePtr++;
     }
-
-    delete[] frameBuffer;
 
     return InputTrack(buffer);
 }

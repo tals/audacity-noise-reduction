31341bafc10028ca8f573e7e4ef1e810e16a1aa6
Jack-42 <jakob-robert@hotmail.de>
2020-12-24 15:02:48 +0100

    Copy dll files using cmake (on windows) (#16)
    And in test.bat, return to project directory in the end

--- build_driver.bat
+++ build_driver.bat
@@ -3,5 +3,4 @@ cd build
 cmake ..
 cmake --build . --target noisereduction_driver
 cd ..
-copy /y "C:\Program Files\Mega-Nerd\libsndfile\bin\libsndfile-1.dll" ".\build\Debug\libsndfile-1.dll"
 PAUSE
